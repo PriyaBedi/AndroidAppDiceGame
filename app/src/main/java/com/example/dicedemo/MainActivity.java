48a70631076e873db8ec89d07220438964fc3df8
priya <Priyabedi98@gmail.com>
2021-02-23 15:22:17 -0500

    Assignment 1

--- app/src/main/java/com/example/dicedemo/MainActivity.java
+++ app/src/main/java/com/example/dicedemo/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Button;
 import android.util.Log;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -19,10 +20,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // we have two images left dice and right dice
-        // link the code to those two image view inside your layout.....(activity main)
+
         final ImageView leftDie = findViewById(R.id.Left_dice);
         final ImageView rightDie = findViewById(R.id.right_dice);
-        Button rollButton = findViewById(R.id.roll_button);
+        Button lower = findViewById(R.id.low_button);
+        Button higher=findViewById(R.id.high_button);
+
+        final TextView res=findViewById(R.id.result);
         //-------------------------------------------------------------------
 
         final int [] diceArray = {R.drawable.dice1,
@@ -34,23 +38,65 @@ public class MainActivity extends AppCompatActivity {
         // we want to pick one of the dices in array randomly and replace it with left and right
         // image view inside your app!
         // Click listener look for an event on your view components.
-       rollButton.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
+        lower.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-               Random rand = new Random();
-               int randLeftDie = rand.nextInt(6);
-               int randRightDie = rand.nextInt(6);
-               Log.d("left die",String.valueOf(randLeftDie));
-               Log.d("right die",String.valueOf(randRightDie));
+                Random rand = new Random();
+                int randLeftDie = rand.nextInt(6);
+                int randRightDie = rand.nextInt(6);
+                Log.d("left die",String.valueOf(randLeftDie));
+                Log.d("right die",String.valueOf(randRightDie));
 
                 leftDie.setImageResource(diceArray[randLeftDie]);
-               rightDie.setImageResource(diceArray[randRightDie]);
-               // we need to create a random integer for picking the dices randomly!
+                rightDie.setImageResource(diceArray[randRightDie]);
+                // we need to create a random integer for picking the dices randomly!
+
+                if(randLeftDie>randRightDie){
+                    res.setText("USER WINS!!");
+
+                }
+                else if(randLeftDie==randRightDie){
+                    res.setText("OOPS!! ITS A TIE!!");
+
+                }
+                else{
+                    res.setText("COMPUTER WINS!!");
+                }
 
 
-           }
-       });
+            }
+        });
+
+        higher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Random rand = new Random();
+                int randLeftDie = rand.nextInt(6);
+                int randRightDie = rand.nextInt(6);
+                Log.d("left die",String.valueOf(randLeftDie));
+                Log.d("right die",String.valueOf(randRightDie));
+
+                leftDie.setImageResource(diceArray[randLeftDie]);
+                rightDie.setImageResource(diceArray[randRightDie]);
+                // we need to create a random integer for picking the dices randomly!
+
+                if(randLeftDie>randRightDie){
+                    res.setText("COMPUTER WINS!!");
+
+                }
+                else if(randLeftDie==randRightDie){
+                    res.setText("OOPS!! ITS A TIE!!");
+
+                }
+                else{
+                    res.setText("USER WINS!!");
+                }
+
+
+            }
+        });
     }
 
 
